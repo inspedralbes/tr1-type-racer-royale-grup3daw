4d5e0245caa3c7ad9fddce32e2cfb06e4da90435
pol <a24polmolmun@inspedralbes.cat>
2025-10-29 12:18:20 +0100

    flujo de pantallas creado

--- frontend/vue-project/src/components/GameEngine.vue
+++ frontend/vue-project/src/components/GameEngine.vue
@@ -2,6 +2,9 @@
 import { ref } from 'vue'
 import Login from './login.vue'
 import Lobby from './lobby.vue'
+import Joc from './joc.vue'
+import Paginafinal from './paginafinal.vue'
+
 
 const nombreJugador = ref('') 
 const etapa = ref('login')    
@@ -10,12 +13,18 @@ function onLogin(nombre) {
   nombreJugador.value = nombre
   etapa.value = 'lobby'  
 }
+function startGame() {
+etapa.value = 'joc'
+}
+
 </script>
 
 <template>
   <div>    
     <Login v-if="etapa === 'login'" @login="onLogin" />
 
-    <Lobby v-else :playerName="nombreJugador" />
+    <Lobby v-else-if = "etapa === 'lobby'" :playerName = "nombreJugador" @startGame="startGame" />
+    
+    <Joc v-else-if="etapa === 'joc'" />
   </div>
 </template>
