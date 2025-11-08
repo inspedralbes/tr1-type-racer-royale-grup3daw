22e579f92e6e18ec96fdf1073e691337104631ca
chuclao <a23izadelesp@inspedralbes.cat>
2025-11-08 17:49:53 +0100

    a

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -2,6 +2,12 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# DEBUG: Print the environment variables to the log
+echo "--- NGINX ENTRYPOINT DEBUG ---"
+echo "ADMINER_USER is: [${ADMINER_USER}]"
+echo "ADMINER_PASSWORD is: [${ADMINER_PASSWORD}]"
+echo "------------------------------"
+
 # Create the .htpasswd file using environment variables
 # The -b flag is for batch mode, -c to create a new file
 htpasswd -bc /etc/nginx/.htpasswd "$ADMINER_USER" "$ADMINER_PASSWORD"
